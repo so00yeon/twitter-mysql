21842e620dadc38a3bf08731dfeb0df00e8058df
soso <hazels201@gmail.com>
2023-04-28 23:56:48 +0900

    first commit

---
+++ src/main/java/com/example/twittermysql/TwitterMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.twittermysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterMysqlApplication.class, args);
+    }
+
+}
