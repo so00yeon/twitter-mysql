fdbd91d787e53f001f6e4bc6c3bd2df7f2018b27
soso <hazels201@gmail.com>
2023-05-04 00:51:48 +0900

    test: add stopwatch

--- src/test/java/com/example/twittermysql/domain/post/PostBulkInsertTest.java
+++ src/test/java/com/example/twittermysql/domain/post/PostBulkInsertTest.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
 
 @SpringBootTest
 public class PostBulkInsertTest {
@@ -18,15 +19,28 @@ public class PostBulkInsertTest {
     @Test
     public void bulkInsert() {
         var easyRandom = PostFixtureFactory.get(
-                3L,
-                LocalDate.of(2023, 1, 1),
+                4L,
+                LocalDate.of(2000, 1, 1),
                 LocalDate.of(2023, 2, 1)
         );
 
-        var posts = IntStream.range(0, 10)
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+
+        var posts = IntStream.range(0, 10000 * 100)
+                .parallel()
                 .mapToObj(i -> easyRandom.nextObject(Post.class))
                 .toList();
 
+        stopWatch.stop();
+        System.out.println("객체 생성 시간: " + stopWatch.getTotalTimeSeconds());
+
+        var queryStopWatch = new StopWatch();
+        queryStopWatch.start();
+
         postRepository.bulkInsert(posts);
+
+        queryStopWatch.stop();
+        System.out.println("DB 인서트 시간: " + queryStopWatch.getTotalTimeSeconds());
     }
 }
